1d171e497f2c2e6313439a58b53a66b27b91021b
Brian Caffey <briancaffey2010@gmail.com>
2021-04-25 18:01:41 -0400

    feat: release 0.0.2

---
+++ src/integ.default.ts
@@ -0,0 +1,9 @@
+import * as cdk from '@aws-cdk/core';
+import { DjangoCdk } from './index';
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'MyStack');
+
+new DjangoCdk(stack, 'Cdk-Sample-Lib', {
+  bucketName: "my-django-cdk-static-files-bucket"
+});
