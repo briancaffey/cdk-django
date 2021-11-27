40b269fb6b26c2aed4d94eeecbbb7f44e2ced223
Brian Caffey <briancaffey2010@gmail.com>
2021-11-27 16:10:43 -0500

    fix issues with frontend container

--- src/docker-ec2.ts
+++ src/docker-ec2.ts
@@ -253,6 +253,7 @@ docker stack deploy --with-registry-auth -c stack.yml stack
     ec2SecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH access');
     ec2SecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP access');
     ec2SecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Allow HTTPS access');
+    ec2SecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(8080), 'For debugging');
 
     // const instance;
     const instance = new ec2.Instance(this, instanceResourceName, {
