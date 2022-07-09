112d38803b6f92c9b23dddfca7c23eb311d63907
briancaffey <briancaffey2010@gmail.com>
2022-07-09 19:44:41 -0400

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'briancaffey',
+  authorAddress: 'briancaffey2010@gmail.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'django-cdk',
+  repositoryUrl: 'git@github.com:briancaffey/django-cdk.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
